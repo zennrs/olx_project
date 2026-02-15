e0a792c9232f003a11982847b8cf889e918b3851
zennrs <@zennrs>
2026-02-15 13:45:27 +0500

    apps qoshildi

--- root/urls.py
+++ root/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for olx_project project.
+URL configuration for root project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
